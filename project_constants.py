66d8bdf09c76dc35b72afac021a547343223b898
Silva Joao <Joao.Silva@tridonic.com>
2024-12-19 09:13:40 +0000

    simplify pyproject and project_constants - was (very) prematurely optimized

--- project_constants.py
+++ project_constants.py
@@ -1,11 +1,3 @@
 import os.path
 
-TOKENS                       = ('jl95terceira','pytools',)
-PROJECT_PATH                 = os.path.split(__file__)[0]
-MODULE_PATH                  = os.path.join(PROJECT_PATH, 'project', 'package')
-SCRIPTS_INSTALLATION_RELPATH = '-'.join(TOKENS)
-PACKAGE                      = '.'.join(TOKENS)
-
-def relpath(path:str):
-
-    return os.path.relpath(path=path, start=PROJECT_PATH)
+PROJECT_PATH = os.path.split(__file__)[0]
