ff7fd6ca8081d0d5cd2abab411e337e631620363
Silva Joao <jl95terceira@gmail.com>
2025-07-31 10:54:25 +0100

    findfile allow search literal (alternatively to only regex as is now)

--- project/package/findfile.py
+++ project/package/findfile.py
@@ -2,17 +2,19 @@ import os
 import os.path
 import re
 
-def do_it(wd      :str,
-          fn_regex:str):
+def do_it(wd:str,
+          fn:str|re.Pattern):
 
-    print(f'Looking in {wd}, where file names match {repr(fn_regex)}')
+    if not isinstance(fn,re.Pattern):
+        fn = re.compile(re.escape(fn))
+    print(f'Looking in {wd}, where file names match {str(fn)}')
     found:list[str] = []
     for dp,dns,fns in os.walk(wd):
 
-        for fn in fns:
+        for fn_ in fns:
 
-            fn_full  = os.path.join(dp, fn)
-            if not re.search(pattern=fn_regex, string=fn_full): continue
+            fn_full  = os.path.join(dp, fn_)
+            if not fn.search(fn_full): continue
             found.append(fn_full)
     
     if found:
@@ -32,17 +34,21 @@ def main():
     class A:
 
         WORKING_DIRECTORY = 'wd'
-        FILE_NAME_REGEX   = 'fnre'
+        FILE_NAME         = 'f'
+        AS_REGEX          = 're'
 
     p = argparse.ArgumentParser(description='Find all files whose name matches a given expression')
     p.add_argument(f'--{A.WORKING_DIRECTORY}',
-                   help   ='working directory - defaults to current',
+                   help='working directory - defaults to current',
                    default='.')
-    p.add_argument(f'{A.FILE_NAME_REGEX}',
-                   help   ='file name regex to consider')
+    p.add_argument(f'{A.FILE_NAME}',
+                   help='file name (full or partial) to search for')
+    p.add_argument(f'--{A.AS_REGEX}',
+                   help='search as regex',
+                   action='store_true')
     get = p.parse_args().__getattribute__
     # do it
-    do_it(wd      =get(A.WORKING_DIRECTORY),
-          fn_regex=get(A.FILE_NAME_REGEX))
+    do_it(wd=get(A.WORKING_DIRECTORY),
+          fn=(lambda v: (v if not get(A.AS_REGEX) else re.compile(v)))(get(A.FILE_NAME)))
 
 if __name__ == '__main__': main()
